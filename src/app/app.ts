e027c024247c66fdd96219846c895919fbda6676
mateocalderonvillavicencio-byte <mateocalderonvillavicencio@gmail.com>
2026-02-20 10:41:39 -0500

    Primer commit - proyecto Angular

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+
+  nuevaTarea: string = '';
+  tareas: string[] = [];
+
+  agregarTarea() {
+    if (this.nuevaTarea.trim() === '') {
+      return;
+    }
+
+    this.tareas.push(this.nuevaTarea);
+    this.nuevaTarea = '';
+  }
+
+}
